6abce2535efba60a9efa6f63cdbfed8bc1f69222
Xiangyang Ju <xiangyang.ju@gmail.com>
2020-04-23 10:23:03 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+
+description="Generate parameters for MC tuning"
+
+setup(
+    name="tuning_sampler",
+    version="0.1.0",
+    description=description,
+    long_description=description,
+    author="Xiangyang Ju",
+    license="Apache License, Version 2.0",
+    keywords=["generator", "MC tuning"],
+    url="https://github.com/xju2/tuning_sampler.git",
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'pyDOE',
+    ],
+    setup_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+    ],
+    scripts=[
+        'scripts/create_diy_configs',
+    ],
+)
